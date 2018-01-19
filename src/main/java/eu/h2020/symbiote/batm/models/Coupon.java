370c2deca8c7bdf63841930f85c109af0daa6e4f
Joao Amsellem <jamsellem@ubiwhere.com>
2018-01-19 14:59:30 +0000

    coupon model

---
+++ src/main/java/eu/h2020/symbiote/batm/models/Coupon.java
@@ -0,0 +1,108 @@
+package eu.h2020.symbiote.batm.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.Id;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Coupon model
+ *
+ * @author jamsellem
+ */
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Coupon {
+    /**
+     * coupon identifier
+     */
+    @Id
+    @NotBlank
+    public String id;
+
+    /**
+     * user history
+     */
+    @NotNull
+    private List<String> usedBy;
+
+    /**
+     * (platformId): Who issued the coupon.
+     */
+    @NotBlank
+    private String issuer;
+
+    /**
+     * (platformId) (optional): Who is the beneficiary of the coupon. This is an optional field and, if left as such, it can be passed around through several platforms
+     */
+    private String beneficiary;
+
+    /**
+     * (federationId): The federation this coupon belongs to
+     */
+    @NotBlank
+    private String fedIdentifier;
+
+    /**
+     * Type of resources being bartered
+     */
+    @NotBlank
+    private String resourceType;
+
+    /**
+     * Expiry date of the coupon
+     */
+    @NotNull
+    private Date expirationDate;
+
+    /**
+     * Boolean indicating if the coupon can be used only once, or several times
+     */
+    @NotNull
+    private Boolean singleUse;
+
+
+
+    public String getId(){ return id;}
+
+    public String getIssuer() { return issuer; }
+
+    public void setIssuer(String issuer) { this.issuer = issuer; }
+
+    public String getBeneficiary() { return beneficiary; }
+
+    public void setBeneficiary(String beneficiary) { this.beneficiary = beneficiary; }
+
+    public String getFedIdentifier() { return fedIdentifier; }
+
+    public void setFedIdentifier(String fedIdentifier) { this.fedIdentifier = fedIdentifier; }
+
+    public String getResourceType() { return resourceType; }
+
+    public void setResourceType(String resourceType) { this.resourceType = resourceType; }
+
+    public Date getExpirationDate() { return expirationDate; }
+
+    public void setExpirationDate(Date expirationDate) { this.expirationDate = expirationDate; }
+
+    public boolean isSingleUse() { return singleUse; }
+
+    public void setSingleUse(boolean singleUse) { this.singleUse = singleUse; }
+
+    public List<String> getUsedByList() {
+        if(usedBy == null){
+            usedBy = new ArrayList<>();
+        }
+        return usedBy;
+    }
+
+    public void addToUsedByList(String usedBy) {
+        if(this.usedBy == null){
+            this.usedBy = new ArrayList<>();
+        }
+        this.usedBy.add(usedBy);
+    }
+}
