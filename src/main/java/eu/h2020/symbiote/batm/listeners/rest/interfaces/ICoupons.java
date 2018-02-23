873f304c394e34e587eed1b2024f8deaf0511a07
Joao Amsellem <jamsellem@ubiwhere.com>
2018-02-23 18:13:28 +0000

    coupon controller interface

--- src/main/java/eu/h2020/symbiote/batm/listeners/rest/interfaces/ICoupons.java
+++ src/main/java/eu/h2020/symbiote/batm/listeners/rest/interfaces/ICoupons.java
@@ -1,5 +1,6 @@
 package eu.h2020.symbiote.batm.listeners.rest.interfaces;
 
+import eu.h2020.symbiote.batm.dto.CouponDTO;
 import eu.h2020.symbiote.batm.models.Coupon;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/coupon")
 public interface ICoupons {
 
-    @PostMapping
-    ResponseEntity<Coupon> getCouponFromOtherPlatform(@RequestBody Coupon coupon);
-
     @GetMapping("/{fedIdentifier}")
-    ResponseEntity<Coupon> getCoupon(@PathVariable String fedIdentifier, @RequestParam("resource") String resourceType);
+    ResponseEntity<CouponDTO> getCoupon(@PathVariable String fedIdentifier, @RequestParam("resource") String resourceType);
 
 }
