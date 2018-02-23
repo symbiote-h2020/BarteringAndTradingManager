47211d63d3e60b98aba95373648c211e7efdd144
Joao Amsellem <jamsellem@ubiwhere.com>
2018-02-23 18:10:15 +0000

    dto

---
+++ src/main/java/eu/h2020/symbiote/batm/dto/BaseModel.java
@@ -0,0 +1,4 @@
+package eu.h2020.symbiote.batm.dto;
+
+public class BaseModel {
+}
---
+++ src/main/java/eu/h2020/symbiote/batm/dto/CouponDTO.java
@@ -0,0 +1,9 @@
+package eu.h2020.symbiote.batm.dto;
+
+import eu.h2020.symbiote.batm.models.Coupon;
+
+public class CouponDTO extends BaseModel {
+    public Coupon coupon;
+    public boolean isNew;
+
+}
